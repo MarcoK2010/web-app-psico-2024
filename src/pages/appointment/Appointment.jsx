5bdd0ef397e317e23f206f00f641b2f79f05e56e
Joaquin Sian <oscarjoaquinsian@gmail.com>
2024-07-22 14:53:51 -0600

    before merig[

--- src/pages/appointment/Appointment.jsx
+++ src/pages/appointment/Appointment.jsx
@@ -4,10 +4,30 @@ import NavBar from "../../components/NavBar";
 import DropDownCalendar from './components/DropDownCalendar';
 import DropDownHour from './components/DropDownHour';
 import FirstSectionForm from './components/FirstSectionForm';
+import axios from "axios";
+
+
 const Appointment = () => {
+  
+const url = "http://127.0.0.1:8000//api/v1";
+
+
+
+const urlAxios = url + "activity/pending/";
+
+
+try {
+  const respose = axios.get(urlAxios);
+  console.log("Respuesta del servidor", respose.data);
+  return respose.data;
+  
+} catch (error) {
+  console.log("Algo salio mal en el login");
+};
+
+
   return (
    <>
-
    <section className='w-full'>
     <NavBar/>
     <FirstSectionForm/>
