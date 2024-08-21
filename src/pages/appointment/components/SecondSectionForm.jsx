afaa3c32cd69b78443052f2de7d7b3e27b7e5900
Marco Kestler <rodrigocoronado168@gmail.com>
2024-08-21 14:05:32 -0600

    chan ges done in lab[

--- src/pages/appointment/components/SecondSectionForm.jsx
+++ src/pages/appointment/components/SecondSectionForm.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import PsychologistCarousel from './PsychologistCarousel'
-
 const SecondSectionForm = () => {
   return (
   <>
-    <PsychologistCarousel/>
+   <div className='block'>
+     <PsychologistCarousel/>
+  
+ 
+   </div>
+      
   </>
   )
 }
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,20 +1,27 @@
 import React from "react"
 import LoginForm from "./components/LoginForm";
 import TextForm from "./components/TextForm";
-
+import Animation from "../../../Animation - 1724101870523.json"
+import Lottie from "lottie-react";
 
 const Login = () => {
   return (
     <>
     <div className="w-full">
+    <div className="fixed">
+           <Lottie animationData={Animation}/>
+        </div>
       <div className="flex justify-evenly flex-wrap-reverse" >
-      
+     
         <LoginForm/>
-      
+       
+       
         <TextForm/>
       
       </div>
+      
     </div>
+   
     </>
   )
 }
